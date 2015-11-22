73f84e7a23624994177322481bd8b82a58cbc90d
vortexkd <chinnapa@Sharats-MacBook-Pro.local>
2015-11-22 13:55:31 +0900

    Scripts from the tutorial
    For the most part these scripts function the same way as they do in the
    demo game.

---
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	private GameController gameController;
+	public int scoreValue;
+	private PlayerController player;
+	void Start(){
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+		if (gameController == null) {
+			Debug.Log ("Cannot find 'Game Controller' script");
+		}
+		GameObject playerControllerObject = GameObject.FindWithTag ("Player");
+		if (playerControllerObject != null) {
+			player = playerControllerObject.GetComponent<PlayerController> ();
+		} 
+		else {
+			Debug.Log ("Cannot find 'Player' script");
+		}
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Boundary") {
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			//gameController.GameOver();
+			player.PlayerCrash();
+		}
+		gameController.AddScore (scoreValue);
+		if (other.tag != "Player") {
+			Destroy (other.gameObject);
+		}
+		Destroy (gameObject);
+	}
+}
---
+++ Space Shooter/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+	public float tumble;
+	public Rigidbody rb;
+	void Start (){
+		rb = GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+
+}
